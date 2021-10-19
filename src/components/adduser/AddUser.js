549fee8b0685a29e9cae24a239c350f9c862bfbd
Clarance Farley <clarancefarley@gmail.com>
2021-10-19 00:14:33 -0700

    add form validation

--- src/components/adduser/AddUser.js
+++ src/components/adduser/AddUser.js
@@ -1,113 +1,113 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect } from "react";
+import { useForm } from "react-hook-form";
 import { AppContext } from "../../AppContext";
 import { v4 as uuidv4 } from "uuid";
 import "./AddUser.css";
 
 const AddUser = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+    reset,
+    setFocus,
+  } = useForm({ mode: "onBlur"});
+
   const { dispatch } = useContext(AppContext);
 
-  const [user, setUser] = useState({
-    firstname: "",
-    lastname: "",
-    email: "",
-    note: "",
-  });
-
-  const resetUser = () => setUser(user);
-
-  const handleChange = (event) => {
-    const { value, name } = event.target;
-
-    setUser({
-      ...user,
-      [name]: value,
-    });
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    const userData = {
-      id: uuidv4(),
-      firstname: user.firstname,
-      lastname: user.lastname,
-      email: user.email,
-      note: user.note,
-    };
-
+  const handleFormData = (userData) => {
+    userData.id = uuidv4();
     dispatch({
       type: "ADD_USER",
       payload: userData,
     });
 
-    resetUser();
-    console.log(userData);
+    reset();
   };
 
+  
+  useEffect(() => {
+    setFocus("firstname");
+  }, [setFocus]);
+
   return (
-    <form onSubmit={handleSubmit} className="form">
+    <form onSubmit={handleSubmit(handleFormData)} className="form">
       <h1 className="form_title">Add User</h1>
 
       <div className="form_body">
         <div className="input_group_wrapper">
           <div className="input_group firstname">
             <label htmlFor="firstname">
-              First Name<span className="required">*</span>
+              First Name<span className="required_icon">*</span>
             </label>
             <input
               type="text"
-              name="firstname"
-              className="input"
-              onChange={handleChange}
-              value={user.firstname}
+              className={`input ${errors.firstname && "error_border"}`}
+              {...register("firstname", {
+                required: "Please provide a first name.",
+              })}
               placeholder="What is your first name?"
             />
+            {errors.firstname && (
+              <small className="error">{errors.firstname.message}</small>
+            )}
           </div>
 
           <div className="input_group lastname">
             <label htmlFor="lastname">
-              Last Name<span className="required">*</span>
+              Last Name<span className="required_icon">*</span>
             </label>
             <input
               type="text"
-              name={"lastname"}
-              className="input"
-              onChange={handleChange}
-              value={user.lastname}
+              className={`input ${errors.lastname && "error_border"}`}
+              {...register("lastname", {
+                required: "Please provide a last name.",
+              })}
               placeholder="What is your last name?"
             />
+            {errors.lastname && (
+              <small className="error">{errors.lastname.message}</small>
+            )}
           </div>
         </div>
-
         <div className="input_group">
           <label htmlFor="email">
-            Email<span className="required">*</span>
+            Email<span className="required_icon">*</span>
           </label>
           <input
-            type="text"
-            name="email"
-            className="input"
-            onChange={handleChange}
-            value={user.email}
-            placeholder="Enter your email."
+            type="email"
+            className={`input ${errors.email && "error_border"}`}
+            {...register("email", {
+              required: "Please provide an email address.",
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                message: "Please provide a valid email address",
+              },
+            })}
+            placeholder="Enter your email address."
           />
+          {errors.email && (
+            <small className="error">{errors.email.message}</small>
+          )}
         </div>
-
         <div className="input_group">
           <label htmlFor="note">
-            Note<span className="required">*</span>
+            Note<span className="required_icon">*</span>
           </label>
           <input
-            type="text"
-            name="note"
-            className="input"
-            onChange={handleChange}
-            value={user.note}
+            type="note"
+            className={`input ${errors.note && "error_border"}`}
+            {...register("note", {
+              required: "Please provide a note.",
+            })}
             placeholder="Please leave a note."
           />
+          {errors.note && (
+            <small className="error">{errors.note.message}</small>
+          )}
         </div>
 
-        <button type="submit" className="adduser_btn">
+        <button type="submit" disabled={!isValid} className={`${!isValid ? "color_disabled" : "color_default"}`}>
           Add User
         </button>
       </div>
